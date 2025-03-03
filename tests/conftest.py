1adbc433c66a1247bd4a2758e10044e01f48fb50
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2025-03-02 22:14:37 -0600

    fix: get questions from json files & delete unnecesary call for open api key

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,34 @@ import pytest
 import os
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from ragas.llms import LangchainLLMWrapper
-from dotenv import load_dotenv
+import json
 
 
 @pytest.fixture
 def langchain_llm__ragas_wrapper():
-    llm = ChatOpenAI(model="gpt-4", temperature=0, openai_api_key=os.environ.get("OPENAI_API_KEY"))
+    llm = ChatOpenAI(model="gpt-4", temperature=0)
     langchain_llm = LangchainLLMWrapper(llm)
     return langchain_llm
 
 @pytest.fixture
 def get_embeddings():
-    return OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+    return OpenAIEmbeddings()
+
+@pytest.fixture
+def get_question():
+
+    def _get_question(file_name, key_name):
+        with open(f"./tests/data/{file_name}.json", "r") as file:
+            data = json.load(file)
+            return str(data["questions"].get(key_name, ""))
+
+    return _get_question
+
+@pytest.fixture
+def get_reference():
+    def _get_reference(file_name, key_name):
+        with open(f"./tests/data/{file_name}.json", "r") as file:
+            data = json.load(file)
+            return str(data["questions"].get(key_name, ""))
+    return _get_reference
+
